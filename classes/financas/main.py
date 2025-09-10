5b334c749ea479a3f798eb05dcc689dded3b2da1
Tiago Reis <120602689+Reiszin1@users.noreply.github.com>
2025-09-10 09:05:56 -0300

    finalização dataclasses

--- classes/financas/main.py
+++ classes/financas/main.py
@@ -1,6 +1,53 @@
 from categoria import Categoria
 from transacao import Transacao
+from utilitarios import cadastrar_categoria, cadastrar_transacao, saldo_total, LISTA_CATEGORIAS, LISTA_TRANSACOES
 
-c = Categoria(nome="Alimentação")
 
-print(f"Categoria criada: {c.nome}")
+# Criação de categorias
+categoria_receitas = cadastrar_categoria('Receitas')
+categoria_contas = cadastrar_categoria('Contas Fixas')
+categoria_lazer = cadastrar_categoria('Lazer')
+categoria_viagens = cadastrar_categoria('Viagens')
+
+# Criação de transações
+cadastrar_transacao(
+    descricao='Salário',
+    valor=5000.00,
+    categoria=categoria_receitas
+)
+
+cadastrar_transacao(
+    descricao='mesada',
+    valor=1500.00,
+    categoria=categoria_receitas
+)
+
+cadastrar_transacao(
+    descricao='Ingressos show',
+    valor=-150.00,
+    categoria=categoria_lazer
+)
+
+cadastrar_transacao(
+    descricao='Conta de Luz',
+    valor=-500.00,
+    categoria=categoria_contas
+)
+
+cadastrar_transacao(
+    descricao='Disney',
+    valor=-500.00,
+    categoria=categoria_viagens
+)
+
+total = saldo_total()
+print(f"Saldo total: R${total:.2f}")
+
+print("\nTransações cadastradas:") 
+for transacao in LISTA_TRANSACOES:
+    transacao.exibir()
+    print("---")
+
+print("\nCategorias cadastradas:")
+for categoria in LISTA_CATEGORIAS:
+    print(f"- {categoria.nome}")
--- classes/financas/transacao.py
+++ classes/financas/transacao.py
@@ -8,6 +8,7 @@ class Transacao:
     categoria: Categoria
 
     def exibir(self):
+        print("Detalhes da transação:")
         print(f"Descrição: {self.descricao}")
         print(f"Valor: R${self.valor:.2f}")
         print(f"Categoria: {self.categoria.nome}")
